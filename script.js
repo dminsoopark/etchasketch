4897f3e5d16c7b3102edb694f20c218c124dc168
Daniel Park <parkd2011@gmail.com>
2022-10-24 17:28:25 -0700

    Declare container

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let i=0, n=16;
 
+const container = document.getElementById("container");
+
 container.innerHTML = 
     `<div class="row">${'<div class="cell">X</div>'.repeat(n)}</div>`
     .repeat(n).replace(/X/g,_=> (i++).toString(n) )
