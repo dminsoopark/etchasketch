9511b487d3b20b2b6da9bf6560942e2174e8091a
Daniel Park <parkd2011@gmail.com>
2022-10-24 17:56:06 -0700

    Create function that makes grid

---
+++ script.js
@@ -0,0 +1,9 @@
+function createGrid(x) {
+    for (let rows = 0; rows < x; rows++) {
+        for (let columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='grid'></div>");
+        };
+    };
+    $(".grid").wdith(960/x);
+    $(".grid").width(960/x);
+}; 
